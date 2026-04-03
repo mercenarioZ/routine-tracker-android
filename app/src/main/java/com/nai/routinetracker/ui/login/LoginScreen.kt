1d74dc77973533c91071ad83adc274086f50016e
mercenarioZ <funnystar320@gmail.com>
2026-04-03 17:29:35 +0700

    chore(ux): add password visual transformation

--- app/src/main/java/com/nai/routinetracker/ui/login/LoginScreen.kt
+++ app/src/main/java/com/nai/routinetracker/ui/login/LoginScreen.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nai.routinetracker.ui.theme.RoutineTrackerTheme
@@ -52,6 +55,8 @@ fun LoginScreen(
             onValueChange = onPasswordChange,
             label = { Text("Password") },
             singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(16.dp))
